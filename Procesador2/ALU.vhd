6f7cceb4c7c14b461821e7ceef4032a4fe7abe06
Carolina <krito_j_g@utp.edu.co>
2016-04-06 17:07:41 -0500

    Se agrega funcionalidad a la ALU y CU

--- Procesador2/ALU.vhd
+++ Procesador2/ALU.vhd
@@ -1,7 +1,18 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
+--ALU soporta las siguientes operaciones
+
+-- ADD : 000000
+-- SUB : 000001
+-- AND : 000010
+-- ANDN (op1 and not op2) : 000011
+-- OR : 000100
+-- ORN : 000101
+-- XOR : 000110
+-- XNOR : 000111
 
 entity ALU is
     Port ( A : in  STD_LOGIC_VECTOR (31 downto 0);
@@ -14,6 +25,40 @@ architecture Behavioral of ALU is
 
 begin
 
+process(ALUOP)
+	begin
+		case ALUOP is
+		
+			when 	"000000" => 
+				Salida <= A + B;
+										
+			when 	"000001" => 
+				Salida <= A - B;
+				
+			when 	"000010" => 
+				Salida <= A and B;
+										
+			when 	"000011" => 
+				Salida <= A and not B;
+			
+			when 	"000100" => 
+				Salida <= A or B;
+										
+			when 	"000101" => 
+				Salida <= A or not B;
+			
+			when 	"000110" => 
+				Salida <= A xor B;
+										
+			when 	"000111" => 
+				Salida <= A xnor B;
+			
+			when others =>
+				Salida <= (others=>'0'); --error
+			
+			end case;
+
+	end process;	
 
 end Behavioral;
 
--- Procesador2/CPU.vhd
+++ Procesador2/CPU.vhd
@@ -2,6 +2,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+-- ADD : 000000
+-- SUB : 000001
+-- AND : 000010
+-- ANDN (op1 and not op2) : 000011
+-- OR : 000100
+-- ORN : 000101
+-- XOR : 000110
+-- XNOR : 000111
 
 entity CPU is
     Port ( op : in  STD_LOGIC_VECTOR (1 downto 0);
@@ -12,7 +20,47 @@ end CPU;
 architecture Behavioral of CPU is
 
 begin
-
+	
+	process(op, op3)
+		begin
+		
+			if(op = "10") then --formato3
+			
+				case op3 is
+				
+					when 	"000000" => --Add
+						salida <= "000000";
+												
+					when 	"000100" => --Sub
+						salida <= "000001";
+						
+					when "000001"	 => -- And
+						salida <= "000010";
+												
+					when "000101"	 => --Andn
+						Salida <= "000011";
+					
+					when "000010"	 => --or
+						Salida <= "000100";
+												
+					when "000110"	 => --orn
+						Salida <= "000101";
+					
+					when "000011"	 => --xor
+						Salida <= "000110";
+												
+					when 	"000111" => --xnor
+						Salida <= "000111";
+					
+					when others =>
+						Salida <= (others=>'0'); --error
+					
+					end case;
+			end if;
+			
+		end process;	
+	
+	
 
 end Behavioral;
 
