61312e1a6372b246c77568a2191efd595caa5a56
Carolina <krito_j_g@utp.edu.co>
2016-04-13 22:24:21 -0500

    Se crea módulos básicos para el procesador3

---
+++ Procesador3/PSR.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity PSR is
+    Port ( nzvc : in  STD_LOGIC_VECTOR (3 downto 0);
+           rst : in  STD_LOGIC;
+           clk : in  STD_LOGIC;
+           carry : out  STD_LOGIC);
+end PSR;
+
+architecture Behavioral of PSR is
+
+begin
+
+	process(rst,clk,nzvc)
+	begin
+		if rst='1' then
+			carry <= '0';
+		else
+			if rising_edge(clk) then
+				--decisión
+			end if;
+		end if;
+	end process;
+	
+end Behavioral;
+
---
+++ Procesador3/PSR_Modifier.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity PSR_Modifier is
+    Port ( ALUOP : in  STD_LOGIC_VECTOR (5 downto 0);
+           ALU_Result : in  STD_LOGIC_VECTOR (31 downto 0);
+           Crs1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Crs2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           nzvc : out  STD_LOGIC_VECTOR (3 downto 0));
+end PSR_Modifier;
+
+architecture Behavioral of PSR_Modifier is
+
+begin
+
+	process(ALUOP, ALU_Result, Crs1, Crs2)
+	begin
+		--Algo
+	end process;
+	
+end Behavioral;
+
